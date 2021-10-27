cd85be3575ebbf5e38b44b07357ce825c11a3e1f
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-27 09:31:35 +0600

    done for now

--- src/App.js
+++ src/App.js
@@ -1,74 +1,82 @@
 import './App.css';
 import { useState } from 'react';
-import { getAuth,signOut, signInWithPopup, GoogleAuthProvider,GithubAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider,getAuth,signInWithPopup,signOut,GithubAuthProvider } from "firebase/auth";
 import initializeAuthentication from './Firebase/firebase.initialize';
 
-
+//function initialization and variable declaration in common
 initializeAuthentication();
+const auth = getAuth();
 const googleProvider = new GoogleAuthProvider();
 const githubProvider=new GithubAuthProvider();
 
 
 function App() {
-const[user,setUser]=useState({});
-const auth = getAuth();
+ 
+ //state to set user 
+ const[user,setUser]=useState({});
 
-const handleGoogleSignIn=()=>{
-signInWithPopup(auth, googleProvider)
-  .then(result => {
-    const {displayName,email,photoURL}=result.user;
+ //segment for handling sign in
+ const handleGoogleSignIn=()=>{
+  signInWithPopup(auth, googleProvider)
+  .then((result) => {
+    const{displayName,email,photoURL}=result.user;
     const loggedInUser={
       name:displayName,
       email:email,
       photo:photoURL
     }
-    setUser(loggedInUser);
-  }).catch((error) => {
-     console.log(error.message)
-  });
-}
-const handleGithubSignIn=()=>{
-signInWithPopup(auth, githubProvider)
-  .then(result => {
-    // The signed-in user info.
+    setUser(loggedInUser)
+  })
+  }
+
+ //segment to handle github sign in 
+ const handleGithubSignIn=()=>{
+ signInWithPopup(auth, githubProvider)
+  .then((result) => {
     const {email,photoURL} = result.user;
     const loggedInUser={
-    email:email,
-    photo:photoURL
+      email:email,
+      photo:photoURL
     }
     setUser(loggedInUser)
-    // ...
   })
-}
-const handleSignOut=()=>{
-signOut(auth).then(() => {
+  }
+
+  //segment to handle sign out
+  const handleSignOut=()=>{
+  signOut(auth).then(() => {
   setUser({})
-}).catch((error) => {
-});
-}
+  })
+  .catch((error) => {
+  // An error happened.
+  });
+  }
+
   return (
   <div className="App">
 
-
-  { !user.name?
-  <div>
-       <button onClick={handleGoogleSignIn}>google sign in</button>
-     <br />
-     <button onClick={handleGithubSignIn}>github sign in</button>
-     <br />
+   {/* conditional button rendering  */}
+   {
+    !user.name?
+     <div>
+       <button onClick={handleGoogleSignIn} >Google Sign In</button>
+       <br/>
+       <button onClick={handleGithubSignIn}>Github Sign In</button>
+       <br/>
      </div>
-     :<button onClick={handleSignOut}> sign out</button>}
-     
-     {
-       user.email &&<div>
-         <h2>Welcome {user.name}</h2>
-         <h3>I know your email which is {user.email}</h3>
-         <img src={user.photo}></img>
-       </div>
-       
-     }
-  </div>
-    
+     : <button onClick={handleSignOut}>Sign Out</button>
+   }
+
+   {/*display information */}
+
+    {user.name&&
+     <div>
+      <h1>Your name:{user.name}</h1>
+      <h3>Your email:{user.email}</h3>
+      <img src={user.photo}></img>
+     </div>
+    }
+   </div>  
   );
 }
 
