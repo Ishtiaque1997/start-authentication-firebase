9174f5e01a7b077bd8d02b5148a701bcbbb04e8d
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-27 07:25:17 +0600

    firebase-setup-done

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCgpvYf6YjD6oWLcy8AEWbd15YVQuhmfbc",
+  authDomain: "simple-firebase-authenti-28e18.firebaseapp.com",
+  projectId: "simple-firebase-authenti-28e18",
+  storageBucket: "simple-firebase-authenti-28e18.appspot.com",
+  messagingSenderId: "865480365827",
+  appId: "1:865480365827:web:84864040c9a38474857b36"
+};
+
+export default firebaseConfig;
