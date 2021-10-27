c6356724f0145d7e5e01bc6adf0453ac7bbce30c
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-27 07:39:58 +0600

    firebase-setup-done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {getAuth,signInWithPopup,GoogleAuthProvider}from 'firebase/auth';
+import initializeAuthentication from './Firebase/firebase.initialize';
+initializeAuthentication();
 
+const provider = new GoogleAuthProvider();
+const handleSignIn=()=>{
+const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+   console.log(result.user)
+  })
+}
 function App() {
   return (
     <div className="App">
-      
+     <button onClick={handleSignIn}>Sign In</button>  
     </div>
   );
 }
